be68e67ca54a18a40c465220a7ea2342040caf46
Julien Midedji <Julien.Midedji@gmail.com>
2017-07-07 08:54:17 +0200

    Add feedbackkasten

---
+++ tutorium.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Feedbackkasten</title>
+        
+        <!-- CSS -->
+        <link rel="stylesheet"  type="text/css" href="css/style.css">
+        <link rel="stylesheet"  type="text/css" href="css/gaestebuchStyle.css">
+        
+        <!-- Meta Tags -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8" /> 
+		
+        <!-- Favicon -->
+        <link rel="icon" href="img/favicon.ico" type="image/gif" size="48x48">
+        
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+        
+        <!-- Google Captcha -->
+        <script src='https://www.google.com/recaptcha/api.js'></script>
+        
+        <!-- Client side validation -->
+        <script src="process/validate-form.js"></script>
+        
+    </head>
+
+    
+    <body>
+        <div class="container-fluid">    
+            
+            <!-- First Page -->
+            <div id="vertical-alignment">
+                
+                <div id="horizontal-alignment">
+
+                    <div class="row caption">
+                        <h1 class="title">Feedbackkasten</h1>
+                    </div>
+                    
+                    <!-- Failure message HIDDEN -->
+                    <div id="fail" class="col-md-offset-4 col-md-4 alert" style="border">
+                        Bitte beweise, dass du ein Mensch bist.
+                    </div>
+                    
+                    <!-- Form Page -->
+                    <div class="col-md-offset-4 col-md-4">
+                        <form id="form" method="post">
+                            <div class="form-group">
+                                <label for="content">Feedback</label>
+                                <textarea class="form-control" id="content" rows="12" name="content" placeholder="Anonyme Kritik an Julien schreiben.
+" required></textarea>
+                            </div>
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-md-10 col-md-offset-1 g-recaptcha" data-sitekey="6LepQyAUAAAAAGTGHOhumDTpFEczGIuvUWUvUiDx"></div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4 col-md-offset-4">
+                                    <button type="submit" class="btn btn-default">Senden</button>
+                                </div>
+                            </div>
+                        </form>
+                        
+                        <?php
+                        
+                        require 'process/phpmailer/PHPMailerAutoload.php';
+                        
+                        
+                        // Used to combat evil user inputs
+                        function fix($data) {
+                            $data = trim($data);
+                            $data = stripslashes($data);
+                            $data = htmlspecialchars($data);
+                            $data = strip_tags($data);
+                            return $data;
+                        }
+                        
+                        // RECAPTCHA
+                        $responseKeys = array("success" => 0);
+                        
+                        if(!empty($_POST)) {
+                            $captcha    = $_POST["g-recaptcha-response"];
+                            $secretKey  = "6LepQyAUAAAAADabd_wC3R9-JrMkJKxUhvLYDMhD";
+                            $ip         = $_SERVER['REMOTE_ADDR'];
+                            $response   = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
+
+                            $responseKeys = json_decode($response, true);
+                        }
+                        
+                        if(intval($responseKeys["success"]) === 1) {
+                         
+                            // *************************************** SEND ************************************************
+                            if(!empty($_POST)) {
+
+                                $mail = new PHPMailer;
+
+                                $mail->isSMTP();
+                                $mail->Host = 'server102.web-hosting.com';
+                                $mail->SMTPAuth = true;
+                                $mail->Username = 'admin@resamvi.de';
+                                $mail->Password = 'spongebob110';
+                                $mail->SMTPSecure = 'tls';
+                                $mail->Port = 587;
+
+                                $mail->setFrom('admin@resamvi.de', 'Feedback');
+                                $mail->addAddress('admin@resamvi.de', 'Julien Midedji');
+                                $mail->addAddress('Julien.Midedji@gmail.com', 'Julien Midedji');
+
+                                $mail->isHTML(true);
+
+                                $mail->Subject = 'Du hast Feedback bekommen';
+                                $mail->Body    = fix($_POST["content"]);
+
+                                if(!$mail->send()) {
+                                    echo 'Message could not be sent.';
+                                    echo 'Mailer Error: ' . $mail->ErrorInfo;
+                                } else {
+                                    echo 'Nachricht wurde gesendet. Vielen Dank für dein Feedback';
+                                }
+                                
+                            }
+                        }
+                        ?>
+                        
+                    </div>
+                </div>
+            </div>    
+        </div>
+    </body>
+</html>
