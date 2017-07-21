27a551db3bab1368382f27fcc1ed456685ada521
Julien Midedji <Julien.Midedji@gmail.com>
2017-07-21 07:43:24 +0200

    Fix sandwich button

--- src/js/filter-blog.js
+++ src/js/filter-blog.js
@@ -1,10 +1,12 @@
-var reload = function() {
+var reload = function(event) {
+    console.log("hi");
     $("#blog-entries").empty();
     entryCount = 0;
     load(); // @see: scroll-blog.js
 };
 
 $(document).ready(function(){
+  console.log("bonesaw ready");
     $('#programming').click(reload); 
     $('#baking').click(reload); 
     $('#misc').click(reload);
